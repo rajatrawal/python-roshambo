5ed4ad77cc4535695e8f4e4b3b6044e7f1939a86
nick.durbin <nick.durbin@gmail.com>
2020-06-19 11:26:49 -0400

    Bonus problem from coding challenge.

---
+++ coding_challenges/strong_to_int.py
@@ -0,0 +1,26 @@
+# Print out all of the strings in the following array that represent a number divisible by 3:
+# [
+#   "five",
+#   "twenty six",
+#   "nine hundred ninety nine,
+#   "twelve",
+#   "eighteen",
+#   "one hundred one",
+#   "fifty two",
+#   "forty one",
+#   "seventy seven",
+#   "six",
+#   "twelve",
+#   "four",
+#   "sixteen"
+# ]
+# The expected output for the above input is:
+# nine hundred ninety nine
+# twelve
+# eighteen
+# six
+# twelve
+# You may use whatever programming language you wish.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+# Resource: https://stackoverflow.com/questions/26977523/converting-number-to-word?fbclid=IwAR3jnhma-vKNHzF5hIDHK0qW4BpVevY7Aa7zptrKlhPZp7tNTa_K6-KPAVQ
